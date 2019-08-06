6bdf96d419cfebf14abf43a49c2924ccef4adca1
Becky Zorn <bczorn@gmail.com>
2019-08-06 00:14:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,9 +4,6 @@ vaild_move
   if between?(0,8)
     TRUE
   elseif position_taken?
-    
-  else
-    
 end    
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
